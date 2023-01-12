6ad8f32a8a872e9274a1ab3cc9f896906b21c42e
testanurag <121380833+testanurag@users.noreply.github.com>
2023-01-12 20:16:38 +0530

    Add files via upload

---
+++ PassByReference.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+void addOne(int& y) // y is bound to the actual object x
+{
+    ++y; // this modifies the actual object x
+}
+
+int main()
+{
+    int x { 5 };
+
+    std::cout << "value = " << x << '\n';
+
+    addOne(x);
+
+    std::cout << "value = " << x << '\n'; // x has been modified
+
+    return 0;
+}
